c99675a257fc7f0d1b3bb3c536290ac04faa822b
ore88 <ore.ajulo@gmail.com>
2022-03-05 15:33:37 +0000

    update flask app

--- project.py
+++ project.py
@@ -1,15 +1,49 @@
 #!/usr/bin/env python
 import os
 
-from flask import Flask
+from typing import Dict
+
+from google.cloud import aiplatform
+from google.protobuf import json_format
+from google.protobuf.struct_pb2 import Value
+
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+def home():
+    return render_template('index.html')
+
+@app.route('/prediction',methods=['POST'])
+def prediction(project: '571997275877', endpoint_id: '5947891713337982976', instance_dict: Dict, 
+                location: str = "us-central1", api_endpoint: str = "us-central1-aiplatform.googleapis.com"):
+    
+    client_options = {"api_endpoint": api_endpoint}
+        
+    client = aiplatform.gapic.PredictionServiceClient(client_options=client_options)
+        
+    instance = json_format.ParseDict(instance_dict, Value())
+
+    instances = [instance]
+
+    parameters_dict = {}
+
+    parameters = json_format.ParseDict(parameters_dict, Value())
+
+    endpoint = client.endpoint_path(project=project, location=location, endpoint=endpoint_id)
+
+    response = client.predict(endpoint=endpoint, instances=instances, parameters=parameters)
+    
+    print("response")
+
+    print(" deployed_model_id:", response.deployed_model_id)
+
+    # See gs://google-cloud-aiplatform/schema/predict/prediction/tabular_classification_1.0.0.yaml for the format of the predictions.
+    
+    predictions = response.predictions
+    for prediction in predictions:
+        print(" prediction:", dict(prediction))
 
 
 if __name__ == "__main__":
